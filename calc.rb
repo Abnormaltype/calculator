d8a9a8f200b6ea54c476e8f51e9b4926ef7d7f1d
Alex Zhidkov <sashazudkov@gmail.com>
2022-09-28 11:27:03 +0300

    initial

--- calc.rb
+++ calc.rb
@@ -2,37 +2,53 @@ def calculator
 
   # Просим пользователя ввести два числа
   while 1
-    puts 'Первое число:'
-    a = STDIN.gets.chomp.to_i # 2.9 -> 2
-    puts 'Второе число:'
-    b = STDIN.gets.chomp.to_i # the same
-    # just in case :)
-    puts a
-    if a.to_s.match(/\d/) && b.to_s.match(/\d/)
+    while 1
+      puts 'Первое число:'
+      a = STDIN.gets.chomp
+      if a !~ /\D/
+        a = a.to_i # 2.9 -> 2
+        break
+      else
+        puts 'This function takes only integer'
+      end
+    end
+
+
+    while 1
+      puts 'Второе число:'
+      b = STDIN.gets.chomp
+      if b !~ /\D/
+        b = b.to_i # the same
+        break
+      else
+        puts 'This function takes only integer'
+      end
+    end
+
       puts 'Выберите операцию (+ - * /)'
       operation = STDIN.gets.chomp
 
-      # Выводим результат с помощью case
       puts 'Результат:'
       case operation
       when '+'
-        puts a + b
+        result = a + b
+        puts result.to_i
       when '-'
-        puts a - b
+        result = a - b
+        puts result.to_i
       when '*'
-        puts a * b
+        result = a * b
+        puts result.to_i
       when '/'
         begin
-          puts a / b
+          result = a / b
+          puts result.to_i
         rescue ZeroDivisionError
           puts 'You can not divide by zero'
         end
       else
         puts 'Incorrect operation'
       end
-    else
-      puts 'This function takes only integer'
-    end
   end
 end
 
